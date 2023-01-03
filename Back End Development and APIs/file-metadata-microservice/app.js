fec75637ab55a7904f671d5215c98e48102966d2
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-03 19:19:24 +0100

    refactor(file-metadata): use of the routes from router

--- Back End Development and APIs/file-metadata-microservice/app.js
+++ Back End Development and APIs/file-metadata-microservice/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
+const routes = require("./src/backend/routes")
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.use("/", routes);
+
 
 module.exports = app
