eb023283926647875e91acf80338e6659d4e3417
Panagiotis Papadopoulos <pano_90@gmx.net>
2023-01-04 13:57:04 +0100

    feat(file-metadata): wrap multer in function to handle errors ourselves required, as we want to return a JSON response, even with errors

--- Back End Development and APIs/file-metadata-microservice/src/backend/controllers/api/multer.js
+++ Back End Development and APIs/file-metadata-microservice/src/backend/controllers/api/multer.js
@@ -3,6 +3,22 @@ const multer  = require("multer");
 const upload = multer({
   strorage: multer.memoryStorage(),
   limits: { fileSize: process.env.MAX_FILESIZE*1e6 }
-});
+}).single(process.env.FORM_FILE_FIELDNAME);
 
-module.exports = upload
+
+
+function uploadHandling(req, res, next) {
+
+  upload(req, res, (error) => {
+
+    if (error instanceof multer.MulterError) {
+      res.status(400).send({ "error": error.message })
+      return;
+    }
+
+    next()
+  })
+
+}
+
+module.exports = uploadHandling
